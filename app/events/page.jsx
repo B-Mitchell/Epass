cc81300612c8a31cd45ec7bb30b068c1b2079a21
B-mitchell <mitchellonuorah77@gmail.com>
2024-03-16 16:02:25 +0100

    updates

--- app/events/page.jsx
+++ app/events/page.jsx
@@ -1,61 +1,3 @@
-/*'use client'
-import React, { useEffect, useState } from 'react'
-import Event_Ticket from './Event_Ticket'
-import supabase from '../supabase'
-import LoadingAnimation from '../components/LoadingAnimation'
-
-const page = () => {
-    const [ loading, setLoading ] = useState(false);
-    const [ datas, setData ] = useState([]);
-
-    const fetchTickets = async () => {
-        setLoading(true);
-        try {
-            let { data , error } = await supabase
-            .from('tickets')
-            .select('*')
-            .order('created_at', { ascending: false });
-            if (error) {
-                console.log('error fetching tickets')
-            } else {
-                console.log(data);
-                setData(data)
-            }
-        } catch (err) {
-            console.error('Error is: ' + err.message);
-        } finally {
-            setLoading(false);
-        }
-    }
-    console.log(datas)
-
-    useEffect(() => {
-        fetchTickets();
-    }, []);
-    
-  return (
-    <div >
-        <p className='text-center font-bold text-[1.4rem] mt-4'>Available Events</p>
-        <p className={`text-center ${loading ? 'mt-10' : null} font-bold italic`}>{ loading ? <LoadingAnimation /> : null }</p>
-        
-        <div className='grid lg:grid-cols-3 grid-cols-2 gap-4 p-2 w-[100%] m-auto mb-7'>
-        {
-            datas.map((ticket) => {
-                return (
-                    <div key={ticket.created_at} className='flex items-center justify-center'>
-                        <Event_Ticket data={ticket}/>
-                    </div>
-                )
-            })
-        }
-        </div>
-        
-    </div>
-  )
-}
-
-export default page
-*/
 'use client'
 import React, { useEffect, useState } from 'react';
 import Event_Ticket from './Event_Ticket';
@@ -67,7 +9,6 @@ const Page = () => {
   const [datas, setData] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [selectedCategory, setSelectedCategory] = useState('');
-
   const fetchTickets = async () => {
     setLoading(true);
     try {
@@ -86,25 +27,18 @@ const Page = () => {
       setLoading(false);
     }
   };
-
   useEffect(() => {
     fetchTickets();
   }, []);
-
   const filterBySearchAndCategory = (ticket) => {
     const nameMatch = searchTerm ? ticket.title?.toLowerCase().includes(searchTerm.toLowerCase()) : true;
     const categoryMatch = selectedCategory ? ticket.typeOfEvent === selectedCategory : true;
     return nameMatch && categoryMatch;
   };
-  
-  
   const handleSearch = () => {
     fetchTickets();
   };
-
- 
   const uniqueTypeOfEvents = [...new Set(datas.map((ticket) => ticket.typeOfEvent))];
-
   return (
     <div>
       <p className='text-center font-bold text-[1.4rem] mt-4'>Available Events</p>
