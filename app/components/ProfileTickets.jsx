0c02cf4b91b828b2e9c432a312f5b418a4e8e2ea
B-mitchell <mitchellonuorah77@gmail.com>
2024-04-05 20:41:25 +0100

    updates

--- app/components/ProfileTickets.jsx
+++ app/components/ProfileTickets.jsx
@@ -10,7 +10,7 @@ const ProfileTickets = (props) => {
     <div className='w-[100%] h-auto pb-1 md:w-[23rem] border border-[#E0BFB8] rounded-lg overflow-hidden hover:scale-105 transition cursor-pointer relative' onClick={() => {router.push(`events/${uuid}`)}}>
   <Image src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/ticketBucket/public/${user_id}_${image}`} alt='event image' className='w-[100%] min-h-[10rem] max-h-[10rem] h-full' width={400} height={400}/>
   <div className='px-2 py-2'>
-    <p className='font-bold text-[1.1rem] uppercase'>{title}</p>
+    <h2 className='font-bold text-[1.1rem] uppercase overflow-hidden whitespace-nowrap text-ellipsis'>{title}</h2>
     <p className='border border-[#E0BFB8] w-fit p-1 rounded-md text-[#E0BFB8] font-bold'>{typeOfEvent}</p>
     <p className='text-[.9rem]'>{address}</p>
     <p>NGN{price}</p>
--- app/events/Event_Ticket.js
+++ app/events/Event_Ticket.js
@@ -12,7 +12,7 @@ const Event_Ticket = (props) => {
       <Image src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/ticketBucket/public/${user_id}_${image}`} alt='event image' className='w-[100%] min-h-[10rem] max-h-[11rem] h-full' width={400} height={400}/>
 
       <div className='px-2 py-2'>
-        <p className='font-bold text-[1.1rem] uppercase'>{title}</p>
+        <h2 className='font-bold text-[1.1rem] uppercase overflow-hidden whitespace-nowrap text-ellipsis'>{title}</h2>
         <p className=' border border-[#E0BFB8] w-fit p-1 rounded-md text-[#E0BFB8] font-bold'>{typeOfEvent}</p>
         <p className='text-[.9rem] absolute top-0 right-0 bg-[#E0BFB8] p-1 rounded-md'>{nops} left</p>
         <p className='text-[.9rem] w-[9rem] whitespace-nowrap overflow-ellipsis'>{address}</p>
