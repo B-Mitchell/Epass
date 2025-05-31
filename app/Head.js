661b63f58987d1562d102a47edd3494917d26b34
B-mitchell <mitchellonuorah77@gmail.com>
2025-05-31 16:25:08 +0100

    added epass png to metadata

---
+++ app/Head.js
@@ -0,0 +1,14 @@
+export default function Head() {
+    return (
+      <>
+        <title>Epass</title>
+        <meta name="description" content="Your favourite ticket app for discovering and buying event tickets." />
+        <meta property="og:title" content="Epass" />
+        <meta property="og:description" content="Your favourite ticket app for discovering and buying event tickets." />
+        <meta property="og:image" content="https://www.e-pass.xyz/Epass.png" />
+        <meta property="og:url" content="https://www.e-pass.xyz/" />
+        <meta property="og:type" content="website" />
+      </>
+    )
+  }
+  
